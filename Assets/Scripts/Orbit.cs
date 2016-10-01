a298cf477be28d9e99f7544b97f8d246e4226d93
Randoro <anton.arnoldsson@hotmail.com>
2016-10-01 16:06:47 +0200

    added camera pivot, touch rotation on camera and scaled up gameobjects

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private bool bDragging;
+    private Vector3 oldPos;
+    private Vector3 panOrigin;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(player.position.x, player.position.y + 15.0f, player.position.z + 10.0f);
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+
+    void LateUpdate()
+    {
+        
+
+        if (Input.GetMouseButton(0))
+        {
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            transform.position = player.position + offset;
+            transform.LookAt(player.position);
+        }
+    }
+}
