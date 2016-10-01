ae124d7766f5437204cfae7d7a9c75815cfff691
Randoro <anton.arnoldsson@hotmail.com>
2016-10-01 15:46:16 +0200

    Made map into prefab, fixed code and simplified things

--- Assets/Scripts/GoogleMap.cs
+++ Assets/Scripts/GoogleMap.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class GoogleMap : MonoBehaviour {
@@ -10,89 +11,73 @@ public class GoogleMap : MonoBehaviour {
         Terrain,
         Hybrid
     }
-
-
-
-    public bool loadOnStart = true;
-    public bool autoLocateCenter = false;
-    public bool usePhoneLocation = false;
+    
     public GoogleMapLocation centerLocation;
-    public int zoom = 13;
+    public int zoom = 16;
     public MapType mapType;
-    public int size = 512;
-    public bool doubleResolution = false;
-    public GoogleMapMarker[] markers;
-    public GoogleMapPath[] paths;
+    public int size = 640;
+    public bool doubleResolution = true;
+    public Color landscapeColor = Color.green;
+    public Color roadColor = Color.white;
+    public Color waterColor = Color.blue;
 
     void Start()
     {
-        if (loadOnStart) Refresh();
+        Refresh();
     }
 
     public void Refresh()
     {
-        if (autoLocateCenter && (markers.Length == 0 && paths.Length == 0))
-        {
-            Debug.LogError("Auto Center will only work if paths or markers are used.");
-        }
         StopCoroutine(_Refresh());
         StartCoroutine(_Refresh());
     }
 
     IEnumerator _Refresh()
     {
+
         var url = "http://maps.googleapis.com/maps/api/staticmap";
         var qs = "";
         
-        if (!autoLocateCenter)
-        {
-            if (centerLocation.address != "")
-                qs += "center=" + WWW.UnEscapeURL(centerLocation.address);
-            else
-            {
-                qs += "center=" + WWW.UnEscapeURL(string.Format("{0},{1}", centerLocation.latitude, centerLocation.longitude));
-            }
+        // (!autoLocateCenter)
+        //{
 
-            qs += "&zoom=" + zoom.ToString();
+
+        //Check for string location
+        if (centerLocation.address != "")
+            qs += "center=" + WWW.UnEscapeURL(centerLocation.address);
+        else
+        {
+            //Otherwise go with decimal location
+            qs += "center=" +
+                  WWW.UnEscapeURL(string.Format("{0},{1}", centerLocation.latitude, centerLocation.longitude));
         }
+
+
+        //Add zoom argument
+        qs += "&zoom=" + zoom.ToString();
+        
+
+        //Add size argument
         qs += "&size=" + WWW.UnEscapeURL(string.Format("{0}x{0}", size));
+
+        //Add scale argument
         qs += "&scale=" + (doubleResolution ? "2" : "1");
+
+        //Add maptype argument
         qs += "&maptype=" + mapType.ToString().ToLower();
-        var usingSensor = false;
-#if UNITY_IPHONE
-		usingSensor = Input.location.isEnabledByUser && Input.location.status == LocationServiceStatus.Running;
-#endif
-        qs += "&sensor=" + (usingSensor ? "true" : "false");
 
-        foreach (var i in markers)
-        {
-            qs += "&markers=" + string.Format("size:{0}|color:{1}|label:{2}", i.size.ToString().ToLower(), i.color, i.label);
-            foreach (var loc in i.locations)
-            {
-                if (loc.address != "")
-                    qs += "|" + WWW.UnEscapeURL(loc.address);
-                else
-                    qs += "|" + WWW.UnEscapeURL(string.Format("{0},{1}", loc.latitude, loc.longitude));
-            }
-        }
 
-        foreach (var i in paths)
-        {
-            qs += "&path=" + string.Format("weight:{0}|color:{1}", i.weight, i.color);
-            if (i.fill) qs += "|fillcolor:" + i.fillColor;
-            foreach (var loc in i.locations)
-            {
-                if (loc.address != "")
-                    qs += "|" + WWW.UnEscapeURL(loc.address);
-                else
-                    qs += "|" + WWW.UnEscapeURL(string.Format("{0},{1}", loc.latitude, loc.longitude));
-            }
-        }
-
+        //Add custom style
+        qs += "&style=feature:all|element:labels|visibility:off";
+        qs += "&style=feature:road|element:geometry|visibility:on|color:"+ColorTypeConverter.ToHTMLRGBHex(roadColor);
+        qs += "&style=feature:landscape|element:geometry.fill|visibility:on|color:"+ColorTypeConverter.ToHTMLRGBHex(landscapeColor);
+        qs += "&style=feature:water|element:geometry.fill|visibility:on|color:"+ColorTypeConverter.ToHTMLRGBHex(waterColor);
+        
+        //Added project key
+        qs += "&key=AIzaSyBhEI422RQO4o7HJKWL2seCkkootwbRMfU";
+        
         WWW req = new WWW(url + "?" + qs);
-        //yield return www;
-        //var req = new WWW("GET", url + "?" + qs, true);
-        //req.Send ();
+        
         while (!req.isDone)
             yield return null;
         if (req.error == null)
@@ -102,22 +87,8 @@ public class GoogleMap : MonoBehaviour {
             GetComponent<Renderer>().material.mainTexture = tex;
         }
     }
+    
 
-
-}
-
-public enum GoogleMapColor
-{
-    black,
-    brown,
-    green,
-    purple,
-    yellow,
-    blue,
-    gray,
-    orange,
-    red,
-    white
 }
 
 [System.Serializable]
@@ -128,28 +99,21 @@ public class GoogleMapLocation
     public float longitude;
 }
 
-[System.Serializable]
-public class GoogleMapMarker
+public static class ColorTypeConverter
 {
-    public enum GoogleMapMarkerSize
+    public static string ToRGBHex(Color c)
     {
-        Tiny,
-        Small,
-        Mid
+        return string.Format("#{0:X2}{1:X2}{2:X2}", ToByte(c.r), ToByte(c.g), ToByte(c.b));
     }
-    public GoogleMapMarkerSize size;
-    public GoogleMapColor color;
-    public string label;
-    public GoogleMapLocation[] locations;
 
-}
+    public static string ToHTMLRGBHex(Color c)
+    {
+        return string.Format("0x{0:X2}{1:X2}{2:X2}", ToByte(c.r), ToByte(c.g), ToByte(c.b));
+    }
 
-[System.Serializable]
-public class GoogleMapPath
-{
-    public int weight = 5;
-    public GoogleMapColor color;
-    public bool fill = false;
-    public GoogleMapColor fillColor;
-    public GoogleMapLocation[] locations;
+    private static byte ToByte(float f)
+    {
+        f = Mathf.Clamp01(f);
+        return (byte)(f * 255);
+    }
 }
