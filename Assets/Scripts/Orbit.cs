b6e10397a02f6937496e95da766332f88b997d7c
Randoro <anton.arnoldsson@hotmail.com>
2016-10-01 16:26:57 +0200

    added editor camera settings

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -5,7 +5,11 @@ public class Orbit : MonoBehaviour {
 
     public float turnSpeed = 0.2f;
     public Transform player;
-    
+
+    private bool bDragging;
+    private Vector3 oldPos;
+    private Vector3 panOrigin;
+
     private Vector3 offset;
 
     void Start()
@@ -21,10 +25,23 @@ public class Orbit : MonoBehaviour {
         {
             // Get movement of the finger since last frame
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            
+
             offset = Quaternion.AngleAxis(touchDeltaPosition.x * turnSpeed, Vector3.up) * offset;
             transform.position = player.position + offset;
             transform.LookAt(player.position);
         }
+
+
+        //For debugging purposes
+#if UNITY_EDITOR
+
+        if (Input.GetMouseButton(0))
+        {
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            transform.position = player.position + offset;
+            transform.LookAt(player.position);
+        }
+
+#endif
     }
 }
