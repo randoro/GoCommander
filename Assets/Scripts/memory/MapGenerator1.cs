1f5f0601e154e69b672916971f74ffb98df17173
lokatt93 <datajocke93@gmail.com>
2016-10-14 13:21:59 +0200

    la till memory
    finns fel som

---
+++ Assets/Scripts/memory/MapGenerator1.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapGenerator1 : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Transform obstaclePrefab;
+    public Vector2 mapSize;
+
+    [Range(0,1)]
+    public float outLinePercent;
+
+    List<Coordinate> tileCoordinates;
+    Queue<Coordinate> shuffledCoordinates;
+
+    public int shuffleSeeed;
+
+    //Random.Range (1,10000)
+    void Start()
+    {
+       shuffleSeeed = Random.Range(1, 10000);
+
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        tileCoordinates = new List<Coordinate>();
+
+        for(int x = 0; x < mapSize.x; x++)
+            for(int y = 0; y < mapSize.y; y++)
+            {
+                tileCoordinates.Add(new Coordinate(x, y));
+            }
+
+        shuffledCoordinates = new Queue<Coordinate>(Utility1.ShuffleArray(tileCoordinates.ToArray(), shuffleSeeed));
+
+        string holderName = "Generated Map1";
+
+        if(transform.FindChild(holderName))
+        {
+            DestroyImmediate(transform.FindChild(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = this.transform;
+
+        for(int x = 0; x < mapSize.x; x++)
+            for(int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePos = CoordToVector(x, y);
+                Transform newTile = Instantiate(tilePrefab, tilePos, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outLinePercent);
+                newTile.parent = mapHolder;      
+            }
+
+        int obstacleCount = 10;
+        for(int i = 0; i < obstacleCount; i++)
+        {
+            Coordinate randomizeCoord = GetRandomCoordinates();
+            Vector3 obstaclePos = CoordToVector(randomizeCoord.x, randomizeCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePos + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+            newObstacle.parent = mapHolder;
+        }
+    }
+
+    public Vector3 CoordToVector(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    public Coordinate GetRandomCoordinates()
+    {
+        Coordinate randomCoordinate = shuffledCoordinates.Dequeue();
+        shuffledCoordinates.Enqueue(randomCoordinate);
+        return randomCoordinate;
+    }
+
+    public struct Coordinate
+    {
+       public int x;
+       public int y;
+       
+    public Coordinate(int x, int y)
+    {
+            this.x = x;
+            this.y = y;
+    }
+    }
+}
---
+++ Assets/Scripts/memory/move.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class move : MonoBehaviour
+{
+    public float maxtime;
+    public float minswipedist;
+    float starttime;
+    float endtime;
+    Vector3 startpos;
+    Vector3 endpos;
+    float swpedict;
+    float swipetime;
+    Vector3 unite;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch toutch = Input.GetTouch(0);
+
+            if (toutch.phase == TouchPhase.Began)
+            {
+                starttime = Time.time;
+                startpos = toutch.position;
+
+
+            }
+            else if (toutch.phase ==TouchPhase.Ended)
+            {
+                endpos= toutch.position;
+                endtime= Time.time;
+                swpedict = (endpos - startpos).magnitude;
+                starttime = endtime - starttime;
+                if (swipetime < maxtime && swpedict > minswipedist)
+                {
+                    swipe();
+                }
+            }
+        }
+
+    }
+    void swipe()
+    {
+        Vector2 dictens = endpos - startpos;
+        if (Mathf.Abs(dictens.x) > Mathf.Abs(dictens.y))
+        {
+            if (dictens.x > 0)
+            {
+                unite.x = 0;
+                unite.z = -1;
+                transform.Translate(unite);
+
+            }
+            if (dictens.x < 0)
+            {
+                unite.x = 0;
+                unite.z = 1;
+                transform.Translate(unite);
+            }
+
+        }
+        else if (Mathf.Abs(dictens.x) < Mathf.Abs(dictens.y))
+        {
+            if (dictens.y > 0)
+            {
+                unite.z = 0;
+                unite.x = 1;
+                transform.Translate(unite);
+            }
+            if (dictens.y < 0)
+            {
+                unite.z = 0;
+                unite.x = -1;
+                transform.Translate(unite);
+            }
+        }
+    }
+}
+
+      
