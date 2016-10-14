f76b1734092abf1d2d5e1f6fcc9d3c1bf5578029
9robin3 <andblom.robin@gmail.com>
2016-10-14 20:54:01 +0200

    Debug/Cheat Movement - main Scene - Main Game - Robin COMMIT

---
+++ Assets/Scripts/DebugMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugMovement : MonoBehaviour {
+
+    public GameObject lat_inputField;
+    public GameObject long_inputField;
+
+    string lat_value;
+    string long_value;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lat_value = lat_inputField.GetComponent<InputField>().text;
+        long_value = long_inputField.GetComponent<InputField>().text;
+    }
+}
