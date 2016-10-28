b5649bd6d92f17fdc326a0b305f52575a5e0bbfb
Randoro <anton.arnoldsson@hotmail.com>
2016-10-28 11:51:04 +0200

    orbit fix

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -26,7 +26,7 @@ public class Orbit : MonoBehaviour {
 
     void LateUpdate()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (Input.touchCount > 0 && Input.touchCount < 2 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             // Get movement of the finger since last frame
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
