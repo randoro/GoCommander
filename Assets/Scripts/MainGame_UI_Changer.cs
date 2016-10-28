5cd037bdfc731cd710b1cbffdcb4f7773fe32afa
9robin3 <andblom.robin@gmail.com>
2016-10-28 17:05:00 +0200

    Main Scene UI, menu and UI change mechanic

---
+++ Assets/Scripts/MainGame_UI_Changer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGame_UI_Changer : MonoBehaviour
+{
+
+    public enum ActiveUI
+    {
+        inGameUI,
+        menuUI
+    }
+    public ActiveUI activeUI;
+
+    public Canvas inGameUI;
+    public Canvas menuUI;
+
+    public GameObject menu_btn;
+    public GameObject back_btn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        activeUI = ActiveUI.inGameUI;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	  switch(activeUI)
+        {
+            case ActiveUI.inGameUI:
+                {
+                    inGameUI.enabled = true;
+                    menuUI.enabled = false;
+                }
+                break;
+            case ActiveUI.menuUI:
+                {
+                    inGameUI.enabled = false;
+                    menuUI.enabled = true;
+
+                }
+                break;
+        }
+	}
+
+    public void MenuBtnClick()
+    {
+        activeUI = ActiveUI.menuUI;
+    }
+
+    public void BackBtnClick()
+    {
+        activeUI = ActiveUI.inGameUI;
+    }
+}
