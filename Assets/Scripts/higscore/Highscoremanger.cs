d8fecfb0036b706a460cf3c2f072527663ecaf0f
lokatt93 <datajocke93@gmail.com>
2016-11-01 11:54:25 +0100

    highscorescene(enbörjan)

---
+++ Assets/Scripts/higscore/Highscoremanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Highscoremanger : MonoBehaviour {
+
+	public Text hightext;
+	public string test;
+	public int qwizscore;
+	public int puzzelscore;
+	public int memoryscore;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		test = "namn efternamn  "+ qwizscore + " nr1 ";
+		hightext.text = test;
+		qwizscore =qwizscore+1;
+	}
+	public void LoadScores(){
+		
+	}
+
+}
