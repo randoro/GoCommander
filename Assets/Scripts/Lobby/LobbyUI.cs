57bc360d17ee3df259c983b9b1bcb861497e4881
9robin3 <andblom.robin@gmail.com>
2016-11-07 15:04:40 +0100

    Lobby Scene + UI

---
+++ Assets/Scripts/Lobby/LobbyUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyUI : MonoBehaviour {
+
+    public GameObject UIBase;
+
+    public GameObject startBtn;
+    public GameObject leaveBtn;
+    public GameObject memberList;
+    public GameObject listElement;
+
+    int amountOfMembers = 10;
+    
+    // Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void StartBtnClick()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+
+    public void LeaveBtnClick()
+    {
+        SceneManager.LoadScene("login");
+    }
+}
