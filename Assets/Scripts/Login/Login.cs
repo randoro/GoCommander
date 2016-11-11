19db472754cabff0601b1325bd4b55fb160a0901
9robin3 <andblom.robin@gmail.com>
2016-11-11 12:19:25 +0100

    Lobby UI Size Fix

--- Assets/Scripts/Login/Login.cs
+++ Assets/Scripts/Login/Login.cs
@@ -64,7 +64,7 @@ public class Login : MonoBehaviour {
             password.GetComponent<InputField>().text = "";
             username.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "Username";
             password.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "Password";
-            SceneManager.LoadScene("mainScene");
+            SceneManager.LoadScene("LobbyScene");
         }
         if (result == "User not found")
         {
