6c43aa82e889e722a73b78a8584cd09f9c7fc623
supergajic <supergajic@gmail.com>
2016-11-14 12:52:07 +0100

    Updated Highscore.cs

--- Assets/Shiftbetween.cs
+++ Assets/Shiftbetween.cs
@@ -2,38 +2,38 @@
 using System.Collections;
 
 public class Shiftbetween : MonoBehaviour { 
-	public Highscoremanger h;
+	//public Highscoremanger h;
 
 
-	// Use this for initialization
-	void Start () {
+	//// Use this for initialization
+	//void Start () {
 		
-	}
+	//}
 	
-	// Update is called once per frame
+	//// Update is called once per frame
 
 
 	
 
-	public void Shift(){
+	//public void Shift(){
 
-	h.switchiro = 0;
-		}
-	public void Shiftq(){
+	//h.switchiro = 0;
+	//	}
+	//public void Shiftq(){
 
-		h.switchiro = 1;
-	}
-	public void Shiftp(){
+	//	h.switchiro = 1;
+	//}
+	//public void Shiftp(){
 
-		h.switchiro = 2;
-	}
-	public void Shiftm(){
+	//	h.switchiro = 2;
+	//}
+	//public void Shiftm(){
 
-		h.switchiro = 3;
-	}public void Shifts(){
+	//	h.switchiro = 3;
+	//}public void Shifts(){
 
-		h.switchiro = 4;
-	}
+	//	h.switchiro = 4;
+	//}
 
 
 }
