318a54c2140f92b37d2caef00392b8d5f298eff9
9robin3 <andblom.robin@gmail.com>
2016-11-23 13:06:31 +0100

    MainScene + Treasure Orbs

---
+++ Assets/Scripts/TreasureOrbs_UI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TreasureOrbs_UI : MonoBehaviour {
+
+    public Button treasureOrbsBtn;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void TreasureOrbsBtnClick()
+    {
+        MainGame_UI_Changer.activeUI = MainGame_UI_Changer.ActiveUI.treasureOrbsUI;
+    }
+
+}
