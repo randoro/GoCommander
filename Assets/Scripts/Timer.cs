6a30cc62ebd0935267786b8c942d4e5ae52413a1
Hanzzon <kvarnbykillen@hotmail.com>
2016-11-30 12:01:17 +0100

    Timer Added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft = 1200.0f;
+
+    public Text timeLeftText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeLeft -= Time.fixedDeltaTime;
+        print(timeLeft);
+
+        timeLeftText.text = ((int)timeLeft/60).ToString();
+
+        if(timeLeft <= 0)
+        {
+            print("GAME IS OVER!!!!");
+
+            timeLeft = 1200.0f;
+
+            LeaveTeam();
+
+            SceneManager.LoadScene("LobbyScene");
+        }
+
+	}
+
+    IEnumerator LeaveTeam()
+    {
+        string getMembersURL = "http://gocommander.sytes.net/scripts/leave_group.php";
+
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", GoogleMap.username);
+        WWW www = new WWW(getMembersURL, form);
+
+        yield return www;
+
+        string result = www.text;
+    }
+}
