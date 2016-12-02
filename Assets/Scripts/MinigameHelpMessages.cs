9409a4013972de1bf608c90bf656e365b06501e6
9robin3 <andblom.robin@gmail.com>
2016-12-02 19:46:03 +0100

    Minigames help screens - added

---
+++ Assets/Scripts/MinigameHelpMessages.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MinigameHelpMessages : MonoBehaviour {
+
+    public GameObject help_UI;
+    public Button ok_button;
+
+    int timer = 550;
+    
+    // Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer--;
+
+        if(timer < 1)
+        {
+            help_UI.SetActive(false);
+            Destroy(help_UI);
+        }
+	}
+
+    public void OkClick()
+    {
+        help_UI.SetActive(false);
+        Destroy(help_UI);
+    }
+}
