3a3019a16cc5237d913b5d8e8f96b5c9e902d0b5
9robin3 <andblom.robin@gmail.com>
2016-12-02 19:07:16 +0100

    Commander Scene Help UI on start

--- Assets/Scripts/commander/MemberButton.cs
+++ Assets/Scripts/commander/MemberButton.cs
@@ -5,11 +5,17 @@ public class MemberButton : MonoBehaviour {
 
     private Canvas memberListCanvas;
     private GameObject canvasGO;
+    public GameObject helpUI;
+    public GameObject okBtn;
+
+    int helpTimer = 1000;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         canvasGO = GameObject.FindGameObjectWithTag("MemberCanvas");
-	}
+        canvasGO.SetActive(false);
+    }
 
     public void ButtonClicked()
     {
@@ -24,5 +30,21 @@ public class MemberButton : MonoBehaviour {
             canvasGO.SetActive(true);
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Orbit>().enabled = false;
         }
+    }
+
+    public void OKBtnClicked()
+    {
+        helpUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        helpTimer--;
+
+        if (helpTimer < 1)
+        {
+            helpUI.SetActive(false);
+        }
+        
     }
 }
