642240fb8c7b095276445da1da5b8332f4e14dd2
9robin3 <andblom.robin@gmail.com>
2016-12-03 12:50:21 +0100

    Minigame Help Messages

---
+++ Assets/Scripts/Sprint/SprintHelpMessage.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SprintHelpMessage : MonoBehaviour {
+
+    public GameObject help_UI;
+    public Button ok_button;
+
+    int timer = 550;
+
+    static bool showMessage = true;
+
+    //static bool stopShowingMessage;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer--;
+
+        if (timer < 1 && showMessage)
+        {
+            help_UI.SetActive(false);
+            Destroy(help_UI);
+            showMessage = false;
+        }
+    }
+
+    public void OkClick()
+    {
+        help_UI.SetActive(false);
+        Destroy(help_UI);
+        showMessage = false;
+    }
+}
--- Assets/Scripts/memory/MemoryHelpMessage.cs
+++ Assets/Scripts/memory/MemoryHelpMessage.cs
@@ -2,37 +2,42 @@
 using System.Collections;
 using UnityEngine.UI;
 
-
-public class MinigameHelpMessages : MonoBehaviour {
+public class MemoryHelpMessage : MonoBehaviour
+{
 
     public GameObject help_UI;
     public Button ok_button;
 
     int timer = 550;
 
+    static bool showMessage = true;
+
     //static bool stopShowingMessage;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         timer--;
 
-        if(timer < 1)
+        if (timer < 1 && showMessage)
         {
             help_UI.SetActive(false);
             Destroy(help_UI);
+            showMessage = false;
         }
-	}
+    }
 
     public void OkClick()
     {
         help_UI.SetActive(false);
         Destroy(help_UI);
+        showMessage = false;
     }
 }
+
